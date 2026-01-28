a89ed9df5daa8ff34c4cd4cd4e7fb341a383a855
fedebindi <bindi.federico@gmail.com>
2026-01-28 09:56:18 +0100

    Third commit

--- chatbot_functions.py
+++ chatbot_functions.py
@@ -1,51 +1,60 @@
-import chromadb
-from sentence_transformers import SentenceTransformer
-import ollama
-import re
-
-def retrieve_law_chunks(user_question, embed_model, collection):
+def retrieve_law_chunks(user_question,
+                        embed_model,
+                        qdrant_client,
+                        collection):
     # STEP A: Embed the question
     query_vector = embed_model.encode(user_question).tolist()
     
     # STEP B: Retrieve relevant law chunks
-    results = collection.query(
-        query_embeddings=[query_vector],
-        n_results=4  # Top 4 most relevant articles
+    results = qdrant_client.query_points(
+        collection_name=collection,
+        query=query_vector,
+        limit=4
+    ).points
+    
+    return "\n\n".join([point.payload['page_content'] for point in results])
+
+def get_llm_response(user_query,
+                     retrieved_context,
+                     chat_history,
+                     gemini_client):
+
+    system_instruction = '''
+        You are an expert in Iraqi legislation.
+        Use the provided CONTEXT to answer the QUESTION.
+        If the answer is not in the context,
+        say you don't know based on the provided documents.
+    '''
+
+    current_prompt = f'''
+        ******************************************************************
+        
+        QUESTION:
+        {user_query}
+
+        ******************************************************************
+
+        CONTEXT:
+        {retrieved_context}
+
+        ******************************************************************
+    '''
+
+    # build the payload for Gemini
+    gemini_history = []
+    for msg in chat_history:
+        gemini_history.append({"role": msg["role"],
+                            "parts": [{"text": msg["content"]}]})
+    full_messages = gemini_history + [{"role": "user", "parts": [{"text": current_prompt}]}]
+
+    # 4. Call Gemini
+    response = gemini_client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=full_messages,
+        config={
+            "system_instruction": system_instruction,
+            "temperature": 0.3, # Lower temperature = more factual for law
+        }
     )
     
-    return "\n\n".join(results['documents'][0])
-
-def get_llm_response(user_query, retrieved_context, chat_history):
-
-    system_instruction = {
-        "role": "system", 
-        "content": '''You are an expert in Iraqi legislation. Use the provided CONTEXT to answer the QUESTION.
-            If the answer is not in the context, say you don't know based on the provided documents.'''
-    }
-
-    # 2. Format the Current Question with its specific Law Context
-    current_prompt = {
-        "role": "user",
-        "content": f'''
-        
-            ******************************************************************
-            
-            QUESTION:
-            {user_query}
-
-            ******************************************************************
-
-            CONTEXT:
-            {retrieved_context}
-
-            ******************************************************************'''
-    }
-
-    # 3. Build the payload: System + History + Current Prompt
-    # We do NOT save 'retrieved_law_text' into the session history!
-    full_messages = [system_instruction] + chat_history + [current_prompt]
-
-    # 4. Call Ollama
-    response = ollama.chat(model='qwen2.5:7b', messages=full_messages)
-    
-    return response['message']['content']
+    return response.text
--- iraqi_law_app.py
+++ iraqi_law_app.py
@@ -1,19 +1,30 @@
 # Set up your imports and your flask app.
 from flask import Flask, render_template, request, jsonify, session, redirect
-import chromadb
 from sentence_transformers import SentenceTransformer
-import ollama
-import re
+from qdrant_client import QdrantClient
 from chatbot_functions import *
+from google import genai
+
+GEMINI_API_KEY = "AIzaSyCTlCxD-HwRKUNKMvyJOqVEQtzxmQk6EJk"
+QDRANT_URL = "https://fc237fd3-4cd9-4a72-91a4-153b5c17a170.europe-west3-0.gcp.cloud.qdrant.io:6333"
+QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.cXFgmSC0KFc-lZFB6Kb_YIY252x0a5stGT5R-RWTOY8"
 
 app = Flask(__name__)
-app.secret_key = "super_secret_iraqi_law_key"
-client = chromadb.PersistentClient(path="./law_database")
+app.secret_key = b'_5#yf23543d2LF4Qsgfh5hh58z\n\xec]/'
 embed_model = SentenceTransformer('BAAI/bge-m3', device='mps') # use 'cpu' if no Mac GPU
-collection = client.get_collection(name="iraq_laws_en") # iraqi_laws_ar
+
+qdrant_client = QdrantClient(
+    url=QDRANT_URL, 
+    api_key=QDRANT_API_KEY,
+)
+collection = "iraqi_laws_en"
+
+gemini_client = genai.Client(api_key=GEMINI_API_KEY)
+
 
 @app.route('/')
 def index():
+    session.clear()
     return render_template('chatbot.html')
 
 @app.route('/reset', methods=['POST'])
@@ -27,16 +38,20 @@ def reset_chat():
 def ask():
 
     history = session.get('history', [])
+    chat_context = session.get('chat_context', '')
     user_input = request.json.get('message')
-    if history == []:
-        chat_context = retrieve_law_chunks(user_input, embed_model, collection) # we only retrieve context once when history is empty
+    if chat_context == '':
+        chat_context = retrieve_law_chunks(user_input,
+                                           embed_model,
+                                           qdrant_client,
+                                           collection) # we only retrieve context once when history is empty
     
-    bot_answer = get_llm_response(user_input, chat_context, history)
+    bot_answer = get_llm_response(user_input, chat_context, history, gemini_client)
 
     history.append({"role": "user", "content": user_input})
-    history.append({"role": "assistant", "content": bot_answer})
+    history.append({"role": "model", "content": bot_answer})
     session['history'] = history[-6:]
-
+    session['chat_context'] = chat_context
     session.modified = True
     
     return jsonify({
@@ -48,7 +63,7 @@ def ask():
 def info_project():
 
     # Return the information to the report page html.
-    return render_template('info_me.html')
+    return render_template('info_project.html')
 
 
 # This page will be the page after the form
